1975dcc0ddd83c2a13159ea7032cbbc593500e32
sincerelyyyash <yta0088@gmail.com>
2024-09-26 00:47:29 +0530

    User model added

---
+++ src/model/user.model.js
@@ -0,0 +1,23 @@
+
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+}, { timestamps: true });
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
