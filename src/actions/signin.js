c31e1f9ddf91f29da3180bac3c40e39345cf4143
mani-rive <mani@rive.com>
2024-09-26 18:13:19 +0530

    added login and signup page

--- src/actions/signin.js
+++ src/actions/signin.js
@@ -1,19 +1,19 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import dbConnect from '../lib/dbConnect';
-import User from '../models/User';
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dbConnect from "../lib/dbConnect";
+import User from "@/model/user.model";
 
 const signInSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
-
 export async function signInAction(data) {
   const parsedData = signInSchema.safeParse(data);
   if (!parsedData.success) {
-    throw new Error(parsedData.error.errors.map((err) => err.message).join(", "));
+    throw new Error(
+      parsedData.error.errors.map((err) => err.message).join(", ")
+    );
   }
 
   const { email, password } = parsedData.data;
@@ -30,10 +30,12 @@ export async function signInAction(data) {
     throw new Error("Invalid email or password");
   }
 
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 
   return {
-    message: 'User signed in successfully',
+    message: "User signed in successfully",
     token,
     user: {
       name: user.name,
